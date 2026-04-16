d4448471f3d9caaeb3d30099fb1eb55a9957ba6f
Tomás <tomas@gmail.com>
2026-04-16 23:34:45 +0100

    Intitial commit

---
+++ src/efeitos.py
@@ -0,0 +1,17 @@
+
+from console_utils import clear_screen, pause
+
+def main():
+    clear_screen()
+    print("Hello, World!")
+    pause()
+
+
+#:
+
+if __name__== '__main__':
+    main()
+    
+
+
+
